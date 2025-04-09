60c0adde2b134861e679692b136e2ed1564a0eeb
TatianAnat <TatianAnat@yandex.ru>
2025-04-09 22:24:41 +0400

    work6 task1

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,6 @@
 public class Main {
     public static void main(String[] args) {
+        // task1
         int age = 20;
         if (age >= 18) {
             System.out.println("Если возраст человека равен " + age + ", то он совершеннолетний");

fdd6c0f3c61168e5fabc6e7654aec90ca1109f07
TatianAnat <TatianAnat@yandex.ru>
2025-04-09 22:23:15 +0400

    work6 task1

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,11 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        int age = 20;
+        if (age >= 18) {
+            System.out.println("Если возраст человека равен " + age + ", то он совершеннолетний");
+        } else {
+            System.out.println("Если возраст человека равен " + age + ", то он не достиг совершеннолетия, нужно немного подождать");
+        }
+    }
 
     }
-}

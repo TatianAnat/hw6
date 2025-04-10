765d43115d24d2e56f1dc1bf20909dce09fc45d6
TatianAnat <TatianAnat@yandex.ru>
2025-04-10 20:58:16 +0400

    work6 task4

--- src/Main.java
+++ src/Main.java
@@ -23,17 +23,14 @@ public class Main {
         }
 
         // task4
-        int age1 = 59;
+        int age1 = 24;
         if (age1 >= 2 && age1 <= 6) {
             System.out.println("Если возраст человека равен " + age1 + ", то ему нужно ходить в детский сад");
-        }
-        if (age1 >= 7 && age1 <= 17) {
+        } else if (age1 >= 7 && age1 <= 17) {
             System.out.println("Если возраст человека равен " + age1 + ", то ему нужно ходить в школу");
-        }
-        if (age1 >= 18 && age1 <= 24) {
+        } else if (age1 >= 18 && age1 <= 24) {
             System.out.println("Если возраст человека равен " + age1 + ", то ему нужно ходить в университет");
-        }
-        if (age1 >= 24) {
+        } else if (age1 >= 24) {
             System.out.println("Если возраст человека равен " + age1 + ", то ему нужно ходить на работу");
         }
         // task5
